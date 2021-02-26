ecea29a0a16894d3a591e4973a23d684ecc53167
Atabey Kaygun <atabey_kaygun@hotmail.com>
2021-02-26 12:06:40 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# numbers
-A very simple terminal game similar to the simple game that came with CASIO CA-910 watches.
+Once upon a time, when I was in secondary school, a class-mate brought his new cool gadget 
+watch which was a [Casio calculator watch][1].  It was a very cool watch, and cooler still,
+it had a very simple game on it.  This is my *approximation* of the game. I think it is
+short and simple enough to make any modifications to make it more playable. Do whatever you
+may want to it, as long as give credit to the original game and this version. Enjoy!
+
+I wrote this on my linux box. You will need a C compiler and the [ncurses][2] library to
+compile.  Use
+
+    gcc -o numbers numbers.c -lncurses
+
+to compile. The defaul configuration is that 'z' decreases, 'x' increases and space cancels.
+Use 'q' to quit the game.
+
+[1]: https://www.reddit.com/r/casio/comments/8zem5h/casio_ca_901/
+[2]: https://en.wikipedia.org/wiki/Ncurses
---
+++ numbers.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
+#include <time.h>
+
+#define LEN 40  // max length of the number list
+#define SPEED 3 // speed at which numbers generated
+                // higher the number, slower the generation
+
+#define INCREASE 'x'
+#define DECREASE 'z'
+#define KILL     ' '
+
+int main(int argc, char *argv[]) {
+
+  char ch, line[LEN+2];
+  unsigned int score = 0;
+  unsigned short buffer[LEN], i, N=0, c=0;
+
+  srand(time(NULL));
+
+  initscr();
+  keypad(stdscr, TRUE);
+  curs_set(FALSE);
+
+  do {
+    if(N == 0 || rand()%SPEED == 0) 
+      buffer[N++] = rand()%10;
+
+    for(i=0;i<N;i++)
+       line[i] = '0' + buffer[i];
+    line[N] = '\0';
+
+    clear(); 
+    refresh();
+    printw("Score: %d\n%c %40s\n", score, c+'0', line);
+    printw("\n'%c' to increase, '%c' to decrease.\n",INCREASE,DECREASE);
+    printw("Space to kill the number at the beginning of the list.\n");
+    printw("'q' ends the game.\n");
+    endwin();
+
+    ch = getch();
+    switch(ch) {
+      case INCREASE: c = (c+1)%10; break;
+      case DECREASE: if(c>0) c--; else c=9; break;
+      case KILL    : if(buffer[0] == c && N > 0 ) {
+	               score += 1;
+		       for(i=0; i<N; i++) 
+		          buffer[i] = buffer[i+1];
+		       buffer[N]=0;
+		       N--;
+		     }; break;
+      default      : break;
+    }
+  } while(ch!='q' && N < LEN);
+
+  clear(); 
+  refresh();
+  endwin();
+  printf("Thanks for playing.\n");
+  exit(0);
+}
