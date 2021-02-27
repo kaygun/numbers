a978839c0f5a92d6bd8f025e42d0a258b6b0c0da
Atabey Kaygun <atabey_kaygun@hotmail.com>
2021-02-28 00:34:52 +0300

    Refactoring

--- numbers.c
+++ numbers.c
@@ -2,35 +2,24 @@
 #include <stdlib.h>
 #include <ncurses.h>
 #include <time.h>
+#include <math.h>
 
 #define LEN 40  // max length of the number list
-#define SPEED 3 // speed at which numbers generated
-                // higher the number, slower the generation
+#define SPEED 3 // between 1 and 10
 
 #define INCREASE 'x'
 #define DECREASE 'z'
 #define KILL     ' '
 
-int main(int argc, char *argv[]) {
+char ch, line[LEN+2];
+unsigned int score = 0;
+unsigned short buffer[LEN], i, N=0, c=0;
 
-  char ch, line[LEN+2];
-  unsigned int score = 0;
-  unsigned short buffer[LEN], i, N=0, c=0;
-
-  srand(time(NULL));
-
-  initscr();
-  keypad(stdscr, TRUE);
-  curs_set(FALSE);
-
-  do {
-    if(N == 0 || rand()%SPEED == 0) 
-      buffer[N++] = rand()%10;
-
-    for(i=0;i<N;i++)
-       line[i] = '0' + buffer[i];
+void draw(void) {
+    for(i=0; i<N; i++)
+      line[i] = '0' + buffer[i];
     line[N] = '\0';
-
+    
     clear(); 
     refresh();
     printw("Score: %d\n%c %40s\n", score, c+'0', line);
@@ -38,26 +27,45 @@ int main(int argc, char *argv[]) {
     printw("Space to kill the number at the beginning of the list.\n");
     printw("'q' ends the game.\n");
     endwin();
+}
+
+void loop(void) {
+  do {
+    if(N == 0 || (rand()%10)<SPEED)
+      buffer[N++] = rand()%10;
+   
+    draw();
 
     ch = getch();
     switch(ch) {
       case INCREASE: c = (c+1)%10; break;
       case DECREASE: if(c>0) c--; else c=9; break;
       case KILL    : if(buffer[0] == c && N > 0 ) {
-	               score += 1;
-		       for(i=0; i<N; i++) 
-		          buffer[i] = buffer[i+1];
-		       buffer[N]=0;
-		       N--;
+			score += 1;
+			for(i=0; i<N; i++) 
+			   buffer[i] = buffer[i+1];
+			buffer[N]=0;
+			N--;
 		     }; break;
       default      : break;
     }
   } while(ch!='q' && N < LEN);
+}  
 
+
+int main(void) {
+  // init the game
+  srand(time(NULL));
+  initscr();
+  curs_set(FALSE);
+  
+  // main loop
+  loop();
+
+  // finish the game
   clear(); 
   refresh();
   endwin();
-
   printf("Thanks for playing.\n");
   exit(0);
 }
